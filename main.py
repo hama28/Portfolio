f4f12f215e552a367478e592180b252b84dadf63
hata_win <mh.mundus@gmail.com>
2020-09-05 23:38:19 +0900

    9/5 QRcoder fin

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask import request, url_for, redirect
+import datetime
+import qrcode
 
 
 app = Flask(__name__)
@@ -12,6 +15,45 @@ def home():
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
+
+
+@app.route('/qrcoder')
+def qrcoder():
+    return render_template('qrcoder.html', qrcodeimage='/static/images/blank.gif')
+
+
+@app.route('/qrcc', methods=['POST'])
+def qrcc():
+    # 生成するQRcodeの詳細設定
+    qr = qrcode.QRCode(
+        box_size=4,
+        border=8,
+        version=12,
+        error_correction=qrcode.constants.ERROR_CORRECT_Q
+    )
+    # フォームのテキストを取得
+    urlqr = request.form.get('urlqr', '')
+    if urlqr == '':
+        return show_msg('URLを入力してから生成ボタンを押してください')
+    # 描画するデータを指定する
+    qr.add_data(urlqr)
+    # QRコードの元データを作る
+    qr.make()
+    # データをImageオブジェクトとして取得
+    img = qr.make_image()
+    # Imageをファイルに保存
+    imgurl = 'static/images/qrcode.png'
+    img.save(imgurl)
+    # 現在日時を取得
+    ctime = str(datetime.datetime.now())
+    # キャッシュの画像が表示されないようにパスの後ろに日時を追加
+    return render_template('qrcoder.html', qrcodeimage=('/' + imgurl + '?' + ctime))
+
+
+
+def show_msg(msg):
+    return render_template('msg.html', msg=msg)
+
 
 
 if __name__ == '__main__':
